4059cdebeed768c4a70abdf0cc5237611db5edc7
Don Sealy <don.sealy@hotmail.com>
2022-02-03 09:28:19 -0800

    push to re-clone on different box
    First swing, pushing to switch to linux as I blew up windows node trying to upgrade :/

--- src/read-data.js
+++ src/read-data.js
@@ -1,14 +1,14 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
 const dynamodb = new AWS.DynamoDB.DocumentClient({
-  apiVersion: '2012-08-10',
-  endpoint: new AWS.Endpoint('http://localhost:8000'),
-  region: 'us-west-2',
+  apiVersion: "2012-08-10",
+  endpoint: new AWS.Endpoint("http://localhost:8000"),
+  region: "us-west-2",
   // what could you do to improve performance?
 });
 
-const tableName = 'SchoolStudents';
-const studentLastNameGsiName = 'studentLastNameGsi';
+const tableName = "SchoolStudents";
+const studentLastNameGsiName = "studentLastNameGsi";
 
 /**
  * The entry point into the lambda
@@ -19,11 +19,34 @@ const studentLastNameGsiName = 'studentLastNameGsi';
  * @param {string} [event.studentLastName]
  */
 exports.handler = (event) => {
+  const docClient = new AWS.DynamoDB.DocumentClient();
+
   // TODO use the AWS.DynamoDB.DocumentClient to write a query against the 'SchoolStudents' table and return the results.
   // The 'SchoolStudents' table key is composed of schoolId (partition key) and studentId (range key).
+  var params = {
+    RequestItems: {
+      tableName: tableName,
+      Key: {
+        schoolId: event.schoolId,
+        studentId: event.studentId,
+      },
+    },
+  };
 
+  try {
+    let result = await docClient.get(params).promise();
+    return {
+      body: JSON.stringify({
+        message: "Executed successfully.",
+        data: result,
+      }),
+    };
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+  
   // TODO (extra credit) if event.studentLastName exists then query using the 'studentLastNameGsi' GSI and return the results.
-
   // TODO (extra credit) limit the amount of records returned in the query to 5 and then implement the logic to return all
   //  pages of records found by the query (uncomment the test which exercises this functionality)
 };
--- src/write-data.js
+++ src/write-data.js
@@ -1,13 +1,13 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
 const dynamodb = new AWS.DynamoDB.DocumentClient({
-  apiVersion: '2012-08-10',
-  endpoint: new AWS.Endpoint('http://localhost:8000'),
-  region: 'us-west-2',
+  apiVersion: "2012-08-10",
+  endpoint: new AWS.Endpoint("http://localhost:8000"),
+  region: "us-west-2",
   // what could you do to improve performance?
 });
 
-const tableName = 'SchoolStudents';
+const tableName = "SchoolStudents";
 
 /**
  * The entry point into the lambda
@@ -21,7 +21,47 @@ const tableName = 'SchoolStudents';
  * @param {string} event.studentGrade
  */
 exports.handler = (event) => {
+  const docClient = new AWS.DynamoDB.DocumentClient();
+
   // TODO validate that all expected attributes are present (assume they are all required)
+  if (event.schoolId.length < 1) throw new Error("No school id given.");
+  
+  if (event.schoolName.length < 1) throw new Error("No school name given.");
+  
+  if (event.studentId.length < 1) throw new Error("No student id given.");
+  
+  if (event.studentFirstName.length < 1) throw new Error("No student first name given.");
+  
+  if (event.studentLastName.length < 1) throw new Error("No student last name given.");
+  
+  if (event.studentGrade.length < 1) throw new Error("No student grade given.");
+
   // TODO use the AWS.DynamoDB.DocumentClient to save the 'SchoolStudent' record
   // The 'SchoolStudents' table key is composed of schoolId (partition key) and studentId (range key).
+  let params = {
+    TableName: tableName,
+    Item: {
+      schoolId: event.schoolId,
+      schoolName: event.schoolName,
+      studentId: event.studentId,
+      studentFirstName: event.studentFirstName,
+      studentLastName: event.studentLastName,
+      studentGrade: event.studentGrade,
+    },
+  };
+
+  try {
+    let result = await docClient.put(params).promise();
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `Student Id ${event.studentId} successfully recorded.`,
+        data: result,
+      }),
+    };
+  } catch (error) {a
+    console.log(error);
+    return error;
+  }
 };
